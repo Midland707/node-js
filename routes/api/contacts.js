289f08bf5f0c669eb29593ad136c35a1aefeaab0
Midland707 <rus14job@ukr.net>
2023-05-12 23:03:33 +0300

    add router getContactById

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -10,4 +10,17 @@ router.get("/", async (req, res, next) => {
   }
 });
 
+router.get("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await contacts.getContactById(id);
+    if (!result) {
+      return res.status(404).json({ message: "Not Found" });
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
 module.exports = router;
