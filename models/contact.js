28075a87f1d3e29850ee5d859f8d60b6adb8530d
Midland707 <rus14job@ukr.net>
2023-05-19 00:51:51 +0300

    add Joi contacts schema

--- models/contact.js
+++ models/contact.js
@@ -1,20 +1,33 @@
 const { Schema, model } = require("mongoose");
+const { mongooseError } = require("../helpers");
+const Joi = require("joi");
 
-const contactSchema = new Schema({
-  name: {
-    type: String,
-    required: [true, "Set name for contact"],
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
   },
-  email: {
-    type: String,
-  },
-  phone: {
-    type: String,
-  },
-  favorite: {
-    type: Boolean,
-    default: false,
-  },
+  { versionKey: false }
+);
+
+contactSchema.post("save", mongooseError);
+
+const addSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string(),
+  phone: Joi.string(),
 });
 
 const Contact = model("contacts", contactSchema);
