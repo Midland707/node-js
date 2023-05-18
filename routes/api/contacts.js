46755a92b9c69feb46faf03ff1f97d02326fa8c1
Midland707 <rus14job@ukr.net>
2023-05-19 02:24:05 +0300

    bugfix error  bad response status

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,12 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 const contactsController = require("../../controllers/contacts-controller");
-const { validateBody } = require("../../middlewares");
+const { validateBody, isValidId } = require("../../middlewares");
 const { schemas } = require("../../models/contact");
 
 router.get("/", contactsController.getListContacts);
 
-router.get("/:id", contactsController.getContactById);
+router.get("/:id", isValidId, contactsController.getContactById);
 
 router.post(
   "/",
@@ -15,14 +15,11 @@ router.post(
   contactsController.addContact
 );
 
-router.delete(
-  "/:id",
-  validateBody(schemas.addSchema),
-  contactsController.removeContact
-);
+router.delete("/:id", isValidId, contactsController.removeContact);
 
 router.put(
   "/:id",
+  isValidId,
   validateBody(schemas.addSchema),
   contactsController.editContact
 );
