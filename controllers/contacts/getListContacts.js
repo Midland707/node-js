c0241ec0cf0116e2eb93fb18cf6a01c156ab4b81
Midland707 <rus14job@ukr.net>
2023-06-03 23:49:48 +0300

    add filter query by field favorite

--- controllers/contacts/getListContacts.js
+++ controllers/contacts/getListContacts.js
@@ -2,9 +2,10 @@ const { Contact } = require("../../models");
 
 const getListContacts = async (req, res) => {
   const { _id: owner } = req.user;
-  const { page = 1, limit = 20 } = req.query;
+  const { page = 1, limit = 20, favorite } = req.query;
+
   res.json(
-    await Contact.find({ owner })
+    await Contact.find(favorite === undefined ? { owner } : { owner, favorite })
       .populate("owner", "email")
       .limit(limit * 1)
       .skip((page - 1) * limit)
