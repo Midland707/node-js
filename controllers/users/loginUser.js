5304ed654fff9c67de804f7314e22377f853e8ca
Midland707 <rus14job@ukr.net>
2023-06-03 17:12:32 +0300

    bugfix response in loginUser

--- controllers/users/loginUser.js
+++ controllers/users/loginUser.js
@@ -21,7 +21,13 @@ const loginUser = async (req, res) => {
     id: user._id,
   };
   const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "24h" });
-  res.json({ token });
+  res.json({
+    token,
+    user: {
+      email: user.email,
+      subscription: user.subscription,
+    },
+  });
 };
 
 module.exports = loginUser;
