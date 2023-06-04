42aee6410702c7e7abe5ad3b5307dae596b726bb
Midland707 <rus14job@ukr.net>
2023-06-04 19:35:10 +0300

    add to updateAvatar function move to  folder public/avatars with rename img

--- controllers/users/updateAvatar.js
+++ controllers/users/updateAvatar.js
@@ -1,20 +1,35 @@
 const { User } = require("../../models");
 const { HttpError } = require("../../helpers");
 const Jimp = require("jimp");
+const fs = require("fs/promises");
+const path = require("path");
+
+const tempDir = path.resolve(process.cwd(), "tmp");
+const avatarsPath = path.resolve("public", "avatars");
 
 const updateAvatar = async (req, res) => {
   const { _id, avatarURL, email } = req.user;
   const avatarName = email.split("@")[0] + "Avatar" + ".jpg";
+  const avatarNewPath = path.join(avatarsPath, avatarName);
 
-  console.log("avatarName = ", avatarName);
-  Jimp.read(req.file.path, (err, avatar) => {
-    if (err) throw err;
-    avatar.resize(250, 250).quality(80).write(req.file.path); // save
+  await Jimp.read(req.file.path)
+    .then((avatar) => {
+      return avatar.resize(250, 250).quality(80).write(req.file.path);
+    })
+    .catch((error) => {
+      throw error.message;
+    });
+
+  await fs.rename(req.file.path, avatarNewPath, (error) => {
+    if (error) throw error;
   });
 
-  //   const result = await User.findByIdAndUpdate(_id, req.body, { new: true });
-  //   if (!result) throw HttpError(404);
-  //   res.json(result);
+  const result = await User.findByIdAndUpdate(
+    _id,
+    { ...req.body, avatarURL: avatarNewPath },
+    { new: true }
+  );
+  if (!result) throw HttpError(404);
 
   res.json({
     avatarURL: avatarURL,
