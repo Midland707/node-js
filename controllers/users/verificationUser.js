f41809071af113e50a9c9062fc24fcf31493b55b
Midland707 <rus14job@ukr.net>
2023-06-10 00:33:04 +0300

    add controler verificationUser

--- controllers/users/verificationUser.js
+++ controllers/users/verificationUser.js
@@ -1,5 +1,21 @@
+const { HttpError } = require("../../helpers");
+const { User } = require("../../models");
+
 const verificationUser = async (req, res) => {
-  res.json("verify");
+  const { verificationToken } = req.params;
+  const result = await User.findOne({ verificationToken });
+  if (!result) throw HttpError(404, "User not found");
+  const { _id } = result;
+  await User.findOneAndUpdate(
+    { _id },
+    { ...req.body, verificationToken: null, verify: true },
+    {
+      new: true,
+    }
+  );
+  res.json({
+    message: "Verification successful",
+  });
 };
 
 module.exports = verificationUser;
