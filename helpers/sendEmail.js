035ef0425f6d0687ca078c10e79e9200849b79a1
Midland707 <rus14job@ukr.net>
2023-06-09 23:40:51 +0300

    refactoring sendEmail to nodemailer

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -1,35 +1,22 @@
-const Mailjet = require("node-mailjet");
+const nodemailer = require("nodemailer");
 require("dotenv").config();
-const { MJ_APIKEY_PUBLIC, MJ_APIKEY_PRIVATE, MJ_SENDER_EMAIL } = process.env;
+const { UKR_NET_EMAIL, UKR_NET_PASS } = process.env;
 
-const mailjet = new Mailjet({
-  apiKey: MJ_APIKEY_PUBLIC,
-  apiSecret: MJ_APIKEY_PRIVATE,
-});
+const nodemailerConfig = {
+  host: "smtp.ukr.net",
+  port: 465,
+  secure: true,
+  auth: {
+    user: UKR_NET_EMAIL,
+    pass: UKR_NET_PASS,
+  },
+};
+
+const transport = nodemailer.createTransport(nodemailerConfig);
 
 const sendEmail = async (data) => {
-  console.log(mailjet.apiKey);
-  await mailjet.post("send", { version: "v3.1" }).request({
-    Messages: [
-      {
-        From: {
-          Email: MJ_SENDER_EMAIL,
-          Name: "Mailjet Pilot",
-        },
-        To: [
-          {
-            Email: data.to,
-            // Name: "passenger 1",
-          },
-        ],
-        Subject: data.subject,
-        // TextPart:
-        //   "Dear passenger 1, welcome to Mailjet! May the delivery force be with you!",
-        HTMLPart: data.html,
-      },
-    ],
-  });
-
+  const email = { ...data, from: UKR_NET_EMAIL };
+  await transport.sendMail(email);
   return true;
 };
 
